d22d40c7c722da75e7b3c24d5c90af145561d2b5
tornadosk <60984219+tornadosk@users.noreply.github.com>
2025-05-15 19:04:44 -0400

    Update authRoutes and other changes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/authRoutes';
+import test from 'node:test';
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-
+app.use('/api/test', authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { login } from '../controllers/authController';
+import { testController } from '../controllers/testController';
 
 const router = Router();
 
 router.post('/login', login);
+
+router.get('/test', testController);
 
 export default router;
